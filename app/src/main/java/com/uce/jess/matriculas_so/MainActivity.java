bb662f95d75245ea816b020d590e05d81fa4ff4c
Fabricio Gavilanes <Fabricio3220@hotmail.com>
2015-03-10 13:50:42 -0500

    Revert "Login y otras cosas"
    This reverts commit 5ef91fd5039f59c0b714bfcb792909bc148b0089.

--- app/src/main/java/com/uce/jess/matriculas_so/MainActivity.java
+++ app/src/main/java/com/uce/jess/matriculas_so/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -18,25 +16,34 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
     }
-
-    public void ubi(View v) {
-        Intent siguiente = new Intent(getApplicationContext(), Ubicacion_Admin.class);
+    public void ubi(View v){
+        Intent siguiente= new Intent(getApplicationContext(),Ubicacion_Admin.class);
         startActivity(siguiente);
     }
-
-    public void prueba(View v) {
-        Intent sigAdmi = new Intent(this, Administracion.class);
+    public void prueba (View v){
+        Intent sigAdmi= new Intent(this,Administracion.class);
         startActivity(sigAdmi);
     }
 
-    public void Ingresar(View v) {
-        EditText editName = (EditText) findViewById(R.id.etextUser);
-        EditText editPas = (EditText) findViewById(R.id.etextpass);
-        if (editName.getText().toString().equals("root") && editPas.getText().toString().equals("root")) {
-            Intent e = new Intent(this, Administracion.class);
-            startActivity(e);
-        } else {
-            Toast.makeText(getApplicationContext(), "El USER o Pass no son correctos ", Toast.LENGTH_LONG).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 }
